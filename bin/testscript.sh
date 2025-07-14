9062946a077cd2cbade37991052cc03e30aba199
FakePhDKo <koms0818@naver.com>
2025-07-14 20:55:41 +0900

    second commit

---
+++ bin/testscript.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cowsay -f eyes EXECUTION
+echo
+echo EXECUTION | boxes -d santa
+
+echo EXECUTION | boxes -d nuke
+
+echo BunSuck is Suck | boxes -d info
+
+echo BumSuck is Gay | boxes -d critical
